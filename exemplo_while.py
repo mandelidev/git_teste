7f39294d3dd028c1636c80e6cc8557168cc3776f
mandelidev <mandeli.dev@gmail.com>
2024-09-17 09:25:13 -0300

    altera exemplo_while.py

--- exemplo_while.py
+++ exemplo_while.py
@@ -1,15 +1,18 @@
 portugues = 'Olá Mundo!'
-ingles = 'Hello World'
-print(' ')
+ingles = 'Hello World!'
+
 while True:
     usuario = input(f'Digite 1 para português e 2 para inglês: ')
     if usuario == '1':
+        print(' ')
         print(portugues)
         break
     elif usuario == '2':
+        print(' ')
         print(ingles)
         break
     else:
+        print(' ')
         print(f'O programa só funciona com 1 e 2. Digite novamente')
         print(' ')
 
